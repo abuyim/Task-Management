04fbcd7cffa49e32d696fbf150e10538c90c449f
Abubeker Yimam <ayimam@engage2excel.com>
2025-09-11 00:37:33 +0300

    Cleanup: remove build and cache files from repo

---
+++ Backend/Task-Management.Application/Mapping/TaskProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Task_Management.Application.Dtos;
+using Task_Management.Domain.Models;
+
+namespace Task_Management.Application.Mapping
+{
+    public class TaskProfile : Profile
+    {
+        public TaskProfile()
+        {
+            CreateMap<TaskItem, TaskDto>().ReverseMap();
+            CreateMap<TaskItem, CreateTaskDto>().ReverseMap();
+            CreateMap<TaskItem, UpdateTaskDto>().ReverseMap();
+        }
+    }
+}
---
+++ Backend/Task-Management.Application/Mapping/UserProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Task_Management.Application.Dtos;
+using Task_Management.Domain.Models;
+
+namespace Task_Management.Application.Mapping
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<User, UserDto>().ReverseMap();
+            CreateMap<User, CreateUserDto>().ReverseMap();
+            CreateMap<User, UpdateUserDto>().ReverseMap();
+
+        }
+    }
+}
